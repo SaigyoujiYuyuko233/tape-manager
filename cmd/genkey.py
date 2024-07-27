1b5cf8739c0531720cc150f773b8e80160b6ef1a
sakurayuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2024-07-27 21:43:41 +0800

    add nonce, print json config

--- cmd/genkey.py
+++ cmd/genkey.py
@@ -2,6 +2,7 @@ from cleo.commands.command import Command
 from cleo.helpers import argument, option
 from getpass import getpass
 
+import json
 import hashlib
 import base64
 
@@ -35,10 +36,19 @@ class GenkeyCommand(Command):
 
         cipher = AES.new(keyPass, AES.MODE_GCM)
         deviceKeyEncBytes, deviceKeyEncMacBytes = cipher.encrypt_and_digest(deviceKeyBytes)
+
+        assert len(cipher.nonce) == 16
         
         deviceKeyEncStr = base64.b64encode(deviceKeyEncBytes).decode('utf-8')
         deviceKeyEncMacStr = base64.b64encode(deviceKeyEncMacBytes).decode('utf-8')
-    
+        deviceKeyEncNonceStr = base64.b64encode(cipher.nonce).decode('utf-8')
+        
+        jsonStr = json.dumps({
+            "encKey": deviceKeyEncStr,
+            "nonce": deviceKeyEncNonceStr,
+            "digits": deviceKeyEncMacStr,
+        }, indent=4)
+
         if isInteractive:
             self.line('')
             self.line('=' * 50)
@@ -46,9 +56,12 @@ class GenkeyCommand(Command):
             self.line("Key Generated")
             self.line('')
             self.line(f"Key: <fg=green>{deviceKeyEncStr}</>")
+            self.line(f"Nonce: <fg=green>{deviceKeyEncNonceStr}</>")
             self.line(f"Digits: <fg=green>{deviceKeyEncMacStr}</>")
+            self.line('')
+            self.line(jsonStr)
             self.line('')
             self.line('=' * 50)
             self.line('')
         else:
-            print(deviceKeyEncStr, end='')
+            print(jsonStr, end='')
